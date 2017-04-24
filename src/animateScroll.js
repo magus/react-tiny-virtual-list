da3b72938723854381a2005f13f65e486340f022
noah <noah@iamnoah.com>
2017-04-23 17:09:48 -0700

    animateScroll function

---
+++ src/animateScroll.js
@@ -0,0 +1,72 @@
+const rAF = window.requestAnimationFrame;
+
+function easeInOutQuad(t, b, c, d) {
+  t /= d / 2;
+  if (t < 1) return c / 2 * t * t + b;
+  t--;
+  return -c / 2 * (t * (t - 2) - 1) + b;
+}
+
+function linear(t, b, c, d) {
+  let p = t / d;
+
+  if (p > 1) return b + c;
+
+  return b + (p * c);
+}
+
+export default function animateScroll(element, to, duration, timing = linear) {
+  const start = element.scrollTop;
+  const change = to - start;
+  const animationStart = +new Date();
+
+  let animating = true;
+  let lastpos = null;
+
+  const promise = new Promise((resolve, reject) => {
+    function finish() {
+      animating = false;
+      resolve();
+    }
+
+    function animateScroll() {
+      if (!animating) return;
+
+      rAF(animateScroll);
+
+      const now = +new Date();
+      const val = Math.floor(timing(now - animationStart, start, change, duration));
+
+      if (lastpos === val) return;
+
+      lastpos = val;
+      element.scrollTop = val;
+
+      if (element.scrollTop === to) {
+        finish();
+      }
+
+      if (now > animationStart + duration) {
+        element.scrollTop = to;
+        finish();
+      }
+
+      if (now > animationStart + duration) {
+        element.scrollTop = to;
+        animating = false;
+        resolve();
+      }
+    }
+
+    rAF(animateScroll);
+  });
+
+  return {
+    then: promise.then.bind(promise),
+    cancel: () => {
+      animating = false;
+    },
+  };
+};
+
+export const animateScrollQuad = (element, to, duration) => animateScroll(element, to, duration, easeInOutQuad);
